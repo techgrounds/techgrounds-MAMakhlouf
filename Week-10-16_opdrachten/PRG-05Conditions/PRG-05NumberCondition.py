cadb6ec496f320cd4707745cd6c08656cf1c7049
Akram <mamakhlouf34@gmail.com>
2023-06-02 14:02:38 +0200

    af

--- Week-10-16_opdrachten/PRG-05Conditions/PRG-05NumberCondition.py
+++ Week-10-16_opdrachten/PRG-05Conditions/PRG-05NumberCondition.py
@@ -1,6 +1,8 @@
+# Keeps the code running in a while loop when the if/elif conditions are met
 while True:
+    ## Convert the input to an integer with int function
     number = int(input("Enter a number: "))
-
+    # set the conditions
     if number > 100:
         print("The number is higher than 100.")
     elif number < 100:
--- Week-10-16_opdrachten/PRG-05Conditions/PRG-05PersonalMessage.py
+++ Week-10-16_opdrachten/PRG-05Conditions/PRG-05PersonalMessage.py
@@ -5,3 +5,4 @@ if user_input == name:
     print("Welcome back " + name + "!")
 else:
     print("Nice to meet you, " + user_input + "!")
+# if statement to compare user_name with my_name. If they match, it means the user has entered my name, and it prints a personalized welcome back message. Otherwise, it assumes it's a different name and prints a generic personalized greeting message.
